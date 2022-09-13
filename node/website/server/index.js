64084098a0324338e29e786c729bcba6f2d45fae
Jacky <jackysusu@gmail.com>
2022-09-13 23:30:15 +0800

    fix words

--- node/website/server/index.js
+++ node/website/server/index.js
@@ -6,7 +6,6 @@ const { PORT } = process.env;
 fastify.get('/', (req, reply) => {
   console.log('process pid =>', process.pid);
   reply.send('Hello World. Container ID: ' + hostname + '.   IP: ' +  process.env.SSH_CONNECTION);
-
 });
 
 fastify.listen(PORT, err => {
