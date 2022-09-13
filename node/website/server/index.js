6b19d52898e7af1b98e472d3600710c64dccea06
Jacky <jackysusu@gmail.com>
2022-09-13 23:40:31 +0800

    fix words

--- node/website/server/index.js
+++ node/website/server/index.js
@@ -1,11 +1,11 @@
 const fastify = require('fastify')();
 const os = require("os");
 const hostname = os.hostname()
-const { PORT } = process.env;
+const { PORT } = process.pid;
 
 fastify.get('/', (req, reply) => {
   console.log('process pid =>', process.pid);
-  reply.send('Hello World. Container ID: ' + hostname + '.   IP: ' +  process.env.SSH_CONNECTION)
+  reply.send('Hello World. Container ID: ' + hostname + '.   IP: ' +  process.env.SSH_CONNECTION);
 });
 
 fastify.listen(PORT, err => {
