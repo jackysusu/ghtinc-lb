6b1988d36a19f0f00b03baaedd1fc9b429f19284
Jacky <jackysusu@gmail.com>
2022-09-13 22:05:13 +0800

    server name

--- node/website/server/index.js
+++ node/website/server/index.js
@@ -1,10 +1,11 @@
 const fastify = require('fastify')();
-
+const os = require("os");
+const hostname = os.hostname()
 const { PORT } = process.env;
 
 fastify.get('/', (req, reply) => {
   console.log('process pid =>', process.pid);
-  reply.send('Hello World ' + process.env.SERVER_NAME );
+  reply.send('Hello World ' + hostname);
 });
 
 fastify.listen(PORT, err => {
