1944f1fb87c27f9c6b9408cf719e93c1fbdd2447
Jacky <jackysusu@gmail.com>
2022-09-15 10:45:10 +0800

    delet show ip

--- node/website/server/index.js
+++ node/website/server/index.js
@@ -1,11 +1,11 @@
 const fastify = require('fastify')();
 const os = require("os");
-const hostname = os.hostname()
+const hostname = os.hostname();
 const { PORT } = process.env;
 
 fastify.get('/', (req, reply) => {
   console.log('process pid =>', process.pid);
-  reply.send('Hello World. Container ID: ' + hostname + '.   IP: ' +  process.env.SSH_CONNECTION);
+  reply.send('Hello World. Container ID: ' + hostname);
 });
 
 fastify.listen(PORT, err => {
